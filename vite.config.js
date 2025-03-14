667dfc61324187b4a2711405b5e8af930c42f71a
SunnatulloValiyev <sunnatillovaliyev769@gmail.com>
2025-03-14 11:30:24 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-ihjmport tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
-g    tailwindcss(),
+    tailwindcss(),
   ],
 })
